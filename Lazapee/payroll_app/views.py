270faea3afec825efa386c876396b9a745afb635
jmb <juanmiguelluna2004@gmail.com>
2025-05-11 15:23:16 +0800

    removed timezone
    wasnt used in code

--- Lazapee/payroll_app/views.py
+++ Lazapee/payroll_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Employee, Payslip, Account
 from django.contrib import messages
-from django.utils import timezone
 
 # Account management methods
 def login(request):
